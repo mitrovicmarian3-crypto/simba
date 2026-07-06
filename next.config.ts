922c841dc28b7d3aa41ecee05505348eca2f3b27
mitrovicmarian3-crypto <mitrovicmarian3@gmail.com>
2026-07-06 12:18:32 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,29 +15,27 @@ const nextConfig: NextConfig = {
   },
 
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'simbafm.at',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.pravatar.cc',
-        port: '',
         pathname: '/**',
       },
     ],
